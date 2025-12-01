322cbf88cf6f431b5176e9455d93e230e818ef90
BERRAG <berragrizlene1977@gmail.com>
2025-12-01 17:29:52 +0100

    exam1.py fait

---
+++ exam1.py
@@ -0,0 +1,34 @@
+def longest_palindrome(s: str) -> str:
+    if not s or len(s) == 0:
+        return ""
+
+    start = 0
+    end = 0
+
+    def expand(left: int, right: int) -> tuple:
+      
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        
+        return left + 1, right - 1
+
+    for i in range(len(s)):
+      
+        left1, right1 = expand(i, i)
+
+        left2, right2 = expand(i, i + 1)
+
+       
+        if right1 - left1 > end - start:
+            start, end = left1, right1
+
+        if right2 - left2 > end - start:
+            start, end = left2, right2
+
+    return s[start:end + 1]
+
+
+s = input("saisir une chaîne : ")
+resultat = longest_palindrome(s)
+print("plus long palindromique :", resultat)
